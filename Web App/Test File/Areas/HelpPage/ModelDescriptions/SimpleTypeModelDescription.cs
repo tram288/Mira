bb8e562f5600d21aeed40cc81fd9c9951e4a6e3e
pflemister31 <pflemister31@gmail.com>
2018-10-26 21:18:11 -0400

    Deleting web app

--- Web App/Test File/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace Test_File.Areas.HelpPage.ModelDescriptions
-{
-    public class SimpleTypeModelDescription : ModelDescription
-    {
-    }
-}
--- Web App/Test File/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Test_File.MvcApplication" Language="C#" %>
